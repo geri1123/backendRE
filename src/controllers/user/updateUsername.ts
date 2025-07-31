5c64d6eab80f53911e97d3304fca887ac420c427
geri1123 <geriscelmeta1@icloud.com>
2025-07-31 14:32:57 +0200

    a

--- src/controllers/user/updateUsername.ts
+++ src/controllers/user/updateUsername.ts
@@ -2,9 +2,15 @@ import { NextFunction, Request, Response } from "express";
 import { UsernameService } from "../../services/userService/UserNameHistory.js";
 import { TooManyRequestsError, UnauthorizedError } from "../../errors/BaseError.js";
 import { changeUsernameSchema } from "../../validators/users/updateUsernameSchema.js";
-import { ZodError } from "zod";
+
 import { handleZodError } from "../../validators/zodErrorFormated.js";
 import { ChangeUsernameBody } from "../../validators/users/updateUsernameSchema.js";
+import { UsernameHistoryRepository } from "../../repositories/usernameHistory/UsernameHistoryRepository.js";
+import { UserRepositoryPrisma } from "../../repositories/user/UserRepositoryPrisma.js";
+
+const userRepo = new UserRepositoryPrisma();
+const usernameHistoryRepo = new UsernameHistoryRepository();
+const usernameService = new UsernameService(userRepo, usernameHistoryRepo);
 export async function changeUsername(
   req: Request<{}, {}, ChangeUsernameBody>,
   res: Response,
@@ -18,13 +24,12 @@ export async function changeUsername(
   try {
     const { username } = changeUsernameSchema.parse(req.body);
 
-    const service = new UsernameService();
-
-    const canUpdate = await service.canUpdateUsername(userId);
+    
+    const canUpdate = await usernameService.canUpdateUsername(userId);
     if (!canUpdate) {
   throw new TooManyRequestsError("You can only update your username once every 10 days.");
 }
-    await service.changeUsername(userId, username);
+    await usernameService.changeUsername(userId, username);
     res.json({ success: true, message: "Username updated successfully." });
   } catch (err) {
     
